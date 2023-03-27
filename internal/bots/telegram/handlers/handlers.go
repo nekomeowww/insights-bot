eed4c4dd8cb5a05069c177934dbd3465a953ae35
Ayaka Neko <neko@ayaka.moe>
2023-03-27 07:56:16 +0700

    fix: panics

--- internal/bots/telegram/handlers/handlers.go
+++ internal/bots/telegram/handlers/handlers.go
@@ -1,11 +1,14 @@
 package handlers
 
 import (
+	"runtime/debug"
+
 	"go.uber.org/fx"
 
 	"github.com/nekomeowww/insights-bot/internal/bots/telegram/dispatcher"
 	"github.com/nekomeowww/insights-bot/internal/bots/telegram/handlers/summarize"
 	"github.com/nekomeowww/insights-bot/pkg/handler"
+	"github.com/nekomeowww/insights-bot/pkg/logger"
 )
 
 func NewModules() fx.Option {
@@ -18,11 +21,14 @@ func NewModules() fx.Option {
 type NewHandlersParam struct {
 	fx.In
 
+	Logger *logger.Logger
+
 	Dispatcher       *dispatcher.Dispatcher
 	SummarizeHandler *summarize.Handler
 }
 
 type Handlers struct {
+	Logger     *logger.Logger
 	Dispatcher *dispatcher.Dispatcher
 
 	MessageHandlers     []handler.HandleFunc
@@ -32,6 +38,7 @@ type Handlers struct {
 func NewHandlers() func(param NewHandlersParam) *Handlers {
 	return func(param NewHandlersParam) *Handlers {
 		return &Handlers{
+			Logger:     param.Logger,
 			Dispatcher: param.Dispatcher,
 			MessageHandlers: []handler.HandleFunc{
 				param.SummarizeHandler.HandleMessage,
@@ -44,10 +51,28 @@ func NewHandlers() func(param NewHandlersParam) *Handlers {
 }
 
 func (h *Handlers) RegisterHandlers() {
-	for _, handler := range h.MessageHandlers {
-		h.Dispatcher.RegisterOneMessageHandler(handler)
+	for _, mh := range h.MessageHandlers {
+		go func(handlerFunc handler.HandleFunc) {
+			defer func() {
+				if err := recover(); err != nil {
+					h.Logger.Errorf("Panic recovered, %v\n%s", err, debug.Stack())
+					return
+				}
+			}()
+
+			h.Dispatcher.RegisterOneMessageHandler(handlerFunc)
+		}(mh)
 	}
-	for _, handler := range h.ChannelPostHandlers {
-		h.Dispatcher.RegisterOneChannelPostHandler(handler)
+	for _, cph := range h.ChannelPostHandlers {
+		go func(handlerFunc handler.HandleFunc) {
+			defer func() {
+				if err := recover(); err != nil {
+					h.Logger.Errorf("Panic recovered, %v\n%s", err, debug.Stack())
+					return
+				}
+			}()
+
+			h.Dispatcher.RegisterOneChannelPostHandler(handlerFunc)
+		}(cph)
 	}
 }

51e389defc5322d336705826a16d562e150c8001
Ayaka Neko <neko@ayaka.moe>
2023-03-31 14:21:40 +0700

    chore: add more tests

--- internal/bots/telegram/handlers/summarize/common_test.go
+++ internal/bots/telegram/handlers/summarize/common_test.go
@@ -1,14 +1,39 @@
 package summarize
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestExtractContentFromURL(t *testing.T) {
-	extractContentFromURL("http://a.b.c")
+	t.Run("NoHost", func(t *testing.T) {
+		assert := assert.New(t)
+		require := require.New(t)
+
+		article, err := extractContentFromURL("https://a.b.c")
+		require.Error(err)
+		require.Nil(article)
+
+		assert.ErrorIs(err, ErrNetworkError)
+		assert.EqualError(err, `failed to get url https://a.b.c, network error: Get "https://a.b.c": dial tcp: lookup a.b.c: no such host`)
+	})
+
+	t.Run("WeChatOfficialAccount", func(t *testing.T) {
+		t.Skip("skip WeChatOfficialAccount, only test it when needed.")
+
+		assert := assert.New(t)
+		require := require.New(t)
+
+		article, err := extractContentFromURL(fmt.Sprintf("https://mp.weixin.qq.com/s/%s", ""))
+		require.NoError(err)
+
+		assert.NotEmpty(article.Title)
+		assert.NotEmpty(article.TextContent)
+	})
 }
 
 func TestContentTypeCheck(t *testing.T) {

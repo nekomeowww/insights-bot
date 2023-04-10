b9c7e495deb52e9dd5838a50f00da8b00346ec19
Garfield Lee <Garfield550@users.noreply.github.com>
2023-04-10 10:05:00 +0000

    fix: use MustSend method

--- internal/bots/telegram/handlers/help/message.go
+++ internal/bots/telegram/handlers/help/message.go
@@ -15,7 +15,7 @@ func (h *Handler) HandleHelpCommand(c *handler.Context) {
 关闭聊天记录回顾（群组） 用法：/disable_recap
 	`)
 	message.ReplyToMessageID = c.Update.Message.MessageID
-	_, err := c.Bot.Request(message)
+	err := c.Bot.MustSend(message)
 	if err != nil {
 		h.Logger.Errorf("failed to send message to telegram... %v", err)
 		return

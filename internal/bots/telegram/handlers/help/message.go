ef0865ae313715fc765bee02358b04b670669134
Garfield Lee <Garfield550@users.noreply.github.com>
2023-04-18 12:59:03 +0000

    refactor: update help message

--- internal/bots/telegram/handlers/help/message.go
+++ internal/bots/telegram/handlers/help/message.go
@@ -7,13 +7,16 @@ import (
 )
 
 func (h *Handler) HandleHelpCommand(c *handler.Context) {
-	message := tgbotapi.NewMessage(c.Update.Message.Chat.ID, `
-你好，欢迎使用 Insights Bot！
-量子速读 用法：/smr <链接>
-聊天回顾 用法：/recap
-开启聊天记录回顾（群组） 用法：/enable_recap
-关闭聊天记录回顾（群组） 用法：/disable_recap
-	`)
+	message := tgbotapi.NewMessage(c.Update.Message.Chat.ID, ""+
+		"你好，欢迎使用 Insights Bot！\n"+
+		"我当前支持这些命令：\n"+
+		"量子速读 用法：/smr <code>&lt;链接&gt;</code>\n"+
+		"聊天回顾 用法：/recap\n"+
+		"<em>以下命令仅在群聊中可用：</em>\n"+
+		"开启聊天记录回顾（需要管理权限） 用法：/enable_recap\n"+
+		"关闭聊天记录回顾（需要管理权限） 用法：/disable_recap"+
+		"")
 	message.ReplyToMessageID = c.Update.Message.MessageID
+	message.ParseMode = "HTML"
 	c.Bot.MustSend(message)
 }

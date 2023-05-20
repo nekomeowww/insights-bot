053b85b853d1cf174a1c677ee181dd31f9e78b27
Ayaka Neko <neko@ayaka.moe>
2023-05-20 14:37:20 +0800

    tests: update

--- pkg/linkprev/linkprev_test.go
+++ pkg/linkprev/linkprev_test.go
@@ -39,9 +39,9 @@ func TestPreview(t *testing.T) {
 			meta, err := NewClient().Preview(context.Background(), "https://twitter.com/GoogleDevEurope/status/1640667303158198272")
 			require.NoError(t, err)
 			assert.Equal(t, Meta{
-				Title: "Google Developers Europe on Twitter: \"🎉 Happy Birthday @golang!\n\nDid you know that 11 years ago today Go 1 was publicly released? Join us in celebrating this day by:\n\n🎁 Checking out local meetups → https://t.co/TCNAZL0oOj\n🎁 Trying out the Go Playground → https://t.co/nnkaugz32x\n\nRT if you are a fellow Gopher! https://t.co/jiE7UTMHll\" / Twitter",
+				Title: "Google for Developers Europe on Twitter: \"🎉 Happy Birthday @golang!\n\nDid you know that 11 years ago today Go 1 was publicly released? Join us in celebrating this day by:\n\n🎁 Checking out local meetups → https://t.co/TCNAZL0oOj\n🎁 Trying out the Go Playground → https://t.co/nnkaugz32x\n\nRT if you are a fellow Gopher! https://t.co/jiE7UTMHll\" / Twitter",
 				OpenGraph: opengraph.OpenGraph{
-					Title:       "Google Developers Europe on Twitter",
+					Title:       "Google for Developers Europe on Twitter",
 					Type:        "article",
 					Image:       "https://pbs.twimg.com/media/FsTSN8nWwAA278D.png:large",
 					URL:         "https://twitter.com/GoogleDevEurope/status/1640667303158198272",
@@ -58,7 +58,7 @@ func TestPreview(t *testing.T) {
 			require.NoError(t, err)
 			assert.Equal(t, Meta{
 				OpenGraph: opengraph.OpenGraph{
-					Title:       "Google Developers Europe (@GoogleDevEurope)",
+					Title:       "Google for Developers Europe (@GoogleDevEurope)",
 					Image:       "https://pbs.twimg.com/media/FsTSN8nWwAA278D.png",
 					Description: "🎉 Happy Birthday @golang!\n\nDid you know that 11 years ago today Go 1 was publicly released? Join us in celebrating this day by:\n\n🎁 Checking out local meetups → https://goo.gle/3zaGgRi\n🎁 Trying out the Go Playground → https://goo.gle/3zaGurC\n\nRT if you are a fellow Gopher!",
 					SiteName:    "FixTweet",
@@ -70,9 +70,9 @@ func TestPreview(t *testing.T) {
 			meta, err := NewClient().Preview(context.Background(), "https://vxtwitter.com/GoogleDevEurope/status/1640667303158198272")
 			require.NoError(t, err)
 			assert.Equal(t, Meta{
-				Title: "Google Developers Europe on Twitter: \"🎉 Happy Birthday @golang!\n\nDid you know that 11 years ago today Go 1 was publicly released? Join us in celebrating this day by:\n\n🎁 Checking out local meetups → https://t.co/TCNAZL0oOj\n🎁 Trying out the Go Playground → https://t.co/nnkaugz32x\n\nRT if you are a fellow Gopher! https://t.co/jiE7UTMHll\" / Twitter",
+				Title: "Google for Developers Europe on Twitter: \"🎉 Happy Birthday @golang!\n\nDid you know that 11 years ago today Go 1 was publicly released? Join us in celebrating this day by:\n\n🎁 Checking out local meetups → https://t.co/TCNAZL0oOj\n🎁 Trying out the Go Playground → https://t.co/nnkaugz32x\n\nRT if you are a fellow Gopher! https://t.co/jiE7UTMHll\" / Twitter",
 				OpenGraph: opengraph.OpenGraph{
-					Title:       "Google Developers Europe on Twitter",
+					Title:       "Google for Developers Europe on Twitter",
 					Type:        "article",
 					Image:       "https://pbs.twimg.com/media/FsTSN8nWwAA278D.png:large",
 					URL:         "https://twitter.com/GoogleDevEurope/status/1640667303158198272",
